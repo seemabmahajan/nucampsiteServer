d620f1b35ded92b7ace7457afa81fda998aefeb4
seemabmahajan <seemabmahajan@gmail.com>
2022-04-10 20:05:18 -0700

    mongoose models + router + schemas

--- app.js
+++ app.js
@@ -1,3 +1,73 @@
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// const mongoose = require('mongoose');
+
+// const url = 'mongodb://localhost:27017/nucampsite';
+// const connect = mongoose.connect(url, {
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useNewUrlParser: true, 
+//     useUnifiedTopology: true
+// });
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+// const campsiteRouter = require('./routes/campsiteRouter');
+// const promotionRouter = require('./routes/promotionRouter');
+// const partnerRouter = require('./routes/partnerRouter');
+
+// var app = express();
+
+// // connect.then(() => console.log('Connected correctly to server'), 
+// //     err => console.log(err)
+// // );
+
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/campsites', campsiteRouter);
+// app.use('/promotions', promotionRouter);
+// app.use('/partners', partnerRouter);
+
+// connect.then(() => console.log('Connected correctly to server'), 
+//     err => console.log(err)
+// );
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,8 +76,24 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 var app = express();
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017/nucampsite';
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+connect.then(() => console.log('Connected correctly to server'), 
+    err => console.log(err)
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +107,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
